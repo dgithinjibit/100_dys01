56f026caac4049fd0ca03621031fbf134178f701
danxpat <dgithinji331@gmail.com>
2026-01-13 23:33:34 +0300

    Refactor paragraph handling and print output
    Removed sentence splitting and added print statement for the paragraph.

--- day01.py
+++ day01.py
@@ -1,9 +1,8 @@
-paragraph = """My name is Daniel, I focus on understanding the syntax of [ython as a DOM language. Understanding data structures is fundamental for designing efficient software and solving complex computational problems."""
-# Split the paragraph into sentences
-sentences = paragraph.split('. ')
-# Print each sentence on a new line
-for sentence in sentences:
-    print(sentence.strip())
+paragraph = """My name is Daniel, I focus on understanding the syntax of python as a DOM language Understanding data structures is fundamental for designing efficient software and solving complex computational problems
+"""
+
+# Print the paragraph
+print(paragraph)
 # adding  numbers
 num1 = 5
 num2 = 1
