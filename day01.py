791d56043ef28451864e59e75d5078430c647f9a
danxpat <dgithinji331@gmail.com>
2026-01-13 23:30:36 +0300

    Add paragraph processing and sum calculation

---
+++ day01.py
@@ -0,0 +1,11 @@
+paragraph = """My name is Daniel, I focus on understanding the syntax of [ython as a DOM language. Understanding data structures is fundamental for designing efficient software and solving complex computational problems."""
+# Split the paragraph into sentences
+sentences = paragraph.split('. ')
+# Print each sentence on a new line
+for sentence in sentences:
+    print(sentence.strip())
+# adding  numbers
+num1 = 5
+num2 = 1
+sum = num1 + num2
+print("The sum of", num1, "and", num2, "is", sum)
